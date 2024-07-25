5b60f913536d15bd317467433bbfd509ac54690d
AxelCArette <axelcarette@gmail.com>
2024-07-25 11:04:51 +0200

    Premier commit

---
+++ src/Controller/QuiSuisJeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class QuiSuisJeController extends AbstractController
+{
+    #[Route('/qui-suis-je', name: 'app_qui_suis_je')]
+    public function index(): Response
+    {
+        return $this->render('qui_suis_je/index.html.twig');
+    }
+}
